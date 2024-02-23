c269c5b3004063b2e4e46e2635e7567d94229c49
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-02-23 23:13:49 +0530

    trying neomorphism

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from 'react-native';
 
 import EditScreenInfo from '../../components/edit-screen-info';
+// import { Neomorph } from 'react-native-neomorph-shadows';
 
 const Page = () => {
   return (
@@ -8,6 +9,27 @@ const Page = () => {
       <Text className={styles.title}>Home</Text>
       <View className={styles.separator} />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <Neomorph
+        inner
+        style={{
+          shadowRadius: 7,
+          borderRadius: 90,
+          backgroundColor: '#F19F9F',
+          width: 180,
+          height: 180,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Neomorph
+          style={{
+            shadowRadius: 7,
+            borderRadius: 50,
+            backgroundColor: '#DDDDDD',
+            width: 100,
+            height: 100,
+          }}
+        />
+      </Neomorph> */}
     </View>
   );
 };
