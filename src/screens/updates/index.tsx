8291041131af8e2540c1f8198b89e0517028e510
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-03-22 11:21:57 +0530

    ✨ feat: updates page for no updates

--- src/screens/updates/index.tsx
+++ src/screens/updates/index.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Image } from 'expo-image';
 
 const Updates = () => {
   return (
-    <View>
-      <Text>Updates</Text>
+    <View className="h-full w-full bg-white justify-center items-center">
+      <Image
+        source={require('../../../assets/illstrations/updates.png')}
+        style={{ height: 120, width: 120 }}
+        contentFit="contain"
+      />
+      <Text className="text-3xl w-[50%] text-center text-text-light/50 font-medium mt-[16]">
+        No Updates for You
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default Updates
+export default Updates;
