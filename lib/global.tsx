1b03ff174f79a8704fb6ee8b41145c4bd4851e8e
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-04-23 20:58:50 +0530

    🔧 Add: Global constants for height and font-size

---
+++ lib/global.tsx
@@ -0,0 +1,11 @@
+import { hp } from './utils/get_screen_dimensions';
+
+export const size = {
+  text: {
+    small: hp(1.2),
+    body: hp(1.6),
+    regular: hp(2.4),
+    lg: hp(3.2),
+    xl: hp(4.0),
+  },
+};
