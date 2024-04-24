c0ede48356fb495eaf8e83f6a4e894d9b48a9b26
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-04-24 12:04:19 +0530

    ♻️  Refactor: Updated Height of text according to the global constants in home screen

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { Image } from 'expo-image';
 import { Ionicons } from '@expo/vector-icons';
+import { size } from '~/lib/global';
+import { hp } from '~/lib/utils/get_screen_dimensions';
 
 type JobCardProps = {
   name: string;
@@ -24,31 +26,47 @@ const JobCard = ({
   logo,
   instantApply,
 }: JobCardProps) => {
+  const { text } = size;
+
   return (
     <View className="p-6 mb-4 w-full bg-neutral-50 rounded-[16] border border-neutral-200 gap-y-6">
       <View className="flex-row gap-x-4 items-center">
-        <Image source={logo} style={{ height: 50, width: 50 }} contentFit="contain" />
+        <Image source={logo} style={{ height: hp(4), width: hp(4) }} contentFit="contain" />
         <View>
-          <Text className="text-3xl font-semibold">{name}</Text>
-          <Text className="text-sm font-light">{company}</Text>
+          <Text className="font-semibold" style={{ fontSize: text.regular }}>
+            {name}
+          </Text>
+          <Text className="font-light" style={{ fontSize: text.small }}>
+            {company}
+          </Text>
         </View>
       </View>
-      <View className="flex-row items-center gap-x-4">
-        <Ionicons name="location" size={24} color="#003A9B" />
-        <Text className="text-xl font-base text-text-light">{location}</Text>
+      <View className="flex-row items-center gap-x-2">
+        <Ionicons name="location" size={16} color="#003A9B" />
+        <Text className="font-base text-text-light" style={{ fontSize: text.body }}>
+          {location}
+        </Text>
       </View>
       <View className="flex-row flex-wrap justify-start w-full gap-4 items-center">
-        <Text className="text-md font-base py-2 px-4 bg-emerald-100 text-emerald-700  rounded-[8]">
+        <Text
+          className="font-base py-2 px-4 bg-emerald-100 text-emerald-700  rounded-[8]"
+          style={{ fontSize: text.body }}>
           ₹ {salary} / month
         </Text>
-        <Text className="text-md font-base py-2 px-4 bg-neutral-200 text-text  rounded-[8]">
+        <Text
+          className="font-base py-2 px-4 bg-neutral-200 text-text  rounded-[8]"
+          style={{ fontSize: text.body }}>
           {time}
         </Text>
-        <Text className="text-md font-base py-2 px-4 bg-neutral-200 text-text  rounded-[8]">
+        <Text
+          className="font-base py-2 px-4 bg-neutral-200 text-text  rounded-[8]"
+          style={{ fontSize: text.body }}>
           {shift}
         </Text>
         {instantApply ? (
-          <Text className="text-md font-base py-2 px-4 bg-sky-100 text-secondary  rounded-[8]">
+          <Text
+            className="font-base py-2 px-4 bg-sky-100 text-secondary  rounded-[8]"
+            style={{ fontSize: text.body }}>
             Instant Apply
           </Text>
         ) : null}
